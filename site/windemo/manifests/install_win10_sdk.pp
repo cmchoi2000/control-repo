bebb45acf18359b2bf34896d5ceb039567563048
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-21 16:28:35 -0700

    update

--- site/windemo/manifests/install_win10_sdk.pp
+++ site/windemo/manifests/install_win10_sdk.pp
@@ -1,5 +1,5 @@
 class windemo::install_win10_sdk {
-  file {'C:\Install\WindowsSDK  ':
+  file {'C:\Install\WindowsSDK':
   ensure => directory,
   source => 'puppet:///win_share/Software/Microsoft/WindowsKits/10/WindowsSDK',
   recurse => 'remote'
@@ -8,6 +8,6 @@ class windemo::install_win10_sdk {
   package {'Windows Software Development Kit - Windows 10.0.16299.91':
   ensure => installed,
   source => 'C:\Install\WindowsSDK\winsdksetup.exe',
-  install_options => ['/features OptionId.WindowsDesktopDebuggers', '/q'],
+  install_options => ['/features', 'OptionId.WindowsDesktopDebuggers', '/q'],
   }
 }
