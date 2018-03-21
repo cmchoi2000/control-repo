aafdef20c1acd4e8e60b3bbd32ec97618185f05b
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-21 13:12:48 -0700

    fix typo

--- site/windemo/manifests/install_msvc_2010_x86.pp
+++ site/windemo/manifests/install_msvc_2010_x86.pp
@@ -1,7 +1,7 @@
 class windemo::install_msvc_2010_x86 {
   file {'C:\Windows\Temp\vcredist_x86.exe':
   ensure => file,
-  source => 'puppet:///win_share/Software/Microsoft/DVS.NET2010/Redist/vcredist_x86.exe',
+  source => 'puppet:///win_share/Software/Microsoft/VS.NET2010/Redist/vcredist_x86.exe',
 
   }
   package {'Microsoft Visual C++ 2010  x86 Redistributable - 10.0.40219':
