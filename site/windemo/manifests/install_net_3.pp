cf58f1f8adf6b5b74ae48de3c152f67b55c7667e
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-21 14:43:03 -0700

    update

--- site/windemo/manifests/install_net_3.pp
+++ site/windemo/manifests/install_net_3.pp
@@ -1,10 +1,5 @@
 class windemo::install_net_3 {
-  file {'C:\Temp\sxs':
-  ensure => directory,
-  source => 'puppet:///win_share/Software/Microsoft/WindowsServer2012R2/sxs',
-  recurse => 'remote'
-  }
-windowsfeature { 'NET-Framework-Core':
-  ensure => present,
-  }
+  windowsfeature { 'NET-Framework-Core':
+    ensure => present,
+    }
 }
