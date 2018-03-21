7d32c0417f1dde8abc00aba65883a9d1b1bb690d
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-21 12:39:32 -0700

    DX11

---
+++ site/windemo/manifests/install_dx11.pp
@@ -0,0 +1,12 @@
+class windemo::install_dx11 {
+  file {'C:\Windows\Temp\DXSDK_Aug09.exe':
+  ensure => file,
+  source => 'puppet:///win_share/Software/Microsoft/DX11/2009-Aug-SDK/DXSDK_Aug09.exe',
+
+  }
+  package {'Microsoft DXSDK_Aug09':
+  ensure => installed,
+  source => 'C:\Windows\Temp\DXSDK_Aug09.exe',
+  install_options => ['/U'],
+  }
+}
