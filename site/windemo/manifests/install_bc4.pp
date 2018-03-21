f3fdb89672f474d07ccac6a3df4bcd1b267d0353
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-21 16:07:51 -0700

    install_bc4

---
+++ site/windemo/manifests/install_bc4.pp
@@ -0,0 +1,16 @@
+class windemo::install_bc4 {
+  file {'C:\Install\bc4  ':
+  ensure => file,
+  source => 'puppet:///win_share/Software/BeyondCompare/BC4/BCompare-4.2.4.22795.exe',
+  }
+  file {'C:\Install\bc4  ':
+  ensure => file,
+  source => 'puppet:///win_share/Software/BeyondCompare/BC4/BC4Key.txt',
+  }
+
+  package {'Beyond Compare 4.2.4':
+  ensure => installed,
+  source => 'C:\Install\bc4\BCompare-4.2.4.22795.exe',
+  install_options => ['/SILENT'],
+  }
+}
