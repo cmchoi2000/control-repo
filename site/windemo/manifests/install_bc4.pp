43feaf1feffaf637bba4c88fa0df1e244377c72f
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-21 16:37:01 -0700

    VERYSILENT

--- site/windemo/manifests/install_bc4.pp
+++ site/windemo/manifests/install_bc4.pp
@@ -1,20 +1,20 @@
 class windemo::install_bc4 {
-	file {'C:\Install\bc4':
+	file {'C:\Install\BC4':
 	ensure => directory,
 	}
 
-  file {'C:\Install\bc4\BCompare-4.2.4.22795.exe':
+  file {'C:\Install\BC4\BCompare-4.2.4.22795.exe':
   ensure => file,
   source => 'puppet:///win_share/Software/BeyondCompare/BC4/BCompare-4.2.4.22795.exe',
   }
-  file {'C:\Install\bc4\BC4Key.txt':
+  file {'C:\Install\BC4\BC4Key.txt':
   ensure => file,
   source => 'puppet:///win_share/Software/BeyondCompare/BC4/BC4Key.txt',
   }
 
   package {'Beyond Compare 4.2.4':
   ensure => installed,
-  source => 'C:\Install\bc4\BCompare-4.2.4.22795.exe',
-  install_options => ['/SILENT'],
+  source => 'C:\Install\BC4\BCompare-4.2.4.22795.exe',
+  install_options => ['/VERYSILENT'],
   }
 }
