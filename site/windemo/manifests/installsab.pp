39cd20ae8bafa3f440664e897c2f7c5b9f4b6228
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-21 17:10:45 -0700

    InstallShield 2016 Standalone Build

--- site/windemo/manifests/installsab.pp
+++ site/windemo/manifests/installsab.pp
@@ -3,7 +3,7 @@ class windemo::installsab {
 #  ensure => file,
 #  source => 'puppet:///win_share/Software/InstallShield/InstallShield 2016/Installs/InstallShield Standalone Build/InstallShield2016StandaloneBuild.exe',
 #  }
-  package {'InstallShield SAB':
+  package {'InstallShield 2016 Standalone Build':
   ensure => installed,
   source => 'C:\Install\InstallShield2016StandaloneBuild.exe',
   install_options => ['/s', '/v"LICENSETYPE=2', 'SERVERNAME=sfs-sw-license', 'ADDLOCAL=ALL', '/qn"'],
