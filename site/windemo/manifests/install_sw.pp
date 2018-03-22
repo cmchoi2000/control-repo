08c3cfcaf9f5c6c59bfc4fe55d21287c321b8770
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-22 14:30:37 -0700

    comment out

--- site/windemo/manifests/install_sw.pp
+++ site/windemo/manifests/install_sw.pp
@@ -5,8 +5,8 @@ class windemo::install_sw {
   #include windemo::set_power_scheme
   include windemo::set_win_update
   include windemo::install_ultraiso
-  include windemo::install_sqlclr2014
-  include windemo::install_smo2014
+  #include windemo::install_sqlclr2014
+  #include windemo::install_smo2014
   include windemo::install_dx11
   include windemo::install_msvc_2010_x64
   include windemo::install_msvc_2010_x86
@@ -14,7 +14,7 @@ class windemo::install_sw {
   include windemo::install_python27
   include windemo::install_perl5
   include windemo::install_putty
-  include windemo::install_sab2016
+  #include windemo::install_sab2016
   include windemo::install_bc4
   include windemo::install_ea10
 }
