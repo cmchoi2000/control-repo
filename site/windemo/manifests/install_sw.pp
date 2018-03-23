317ebcacd76957f26cf86746cd18b7935a0d5af8
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-22 20:34:43 -0700

    fix order

--- site/windemo/manifests/install_sw.pp
+++ site/windemo/manifests/install_sw.pp
@@ -19,7 +19,7 @@ class windemo::install_sw {
   include windemo::install_ea10
 
    Class['windemo::enable_rdp'] -> Class['windemo::disable_uac_reg'] -> Class['windemo::enable_net_3'] -> Class['windemo::set_win_update'] ->
-   Class['windemo::install_ultraiso'] -> Class['windemo::install_dx11'] -> Class['windemo::install_msvc_2010_x64'] ->
-   Class['windemo::install_msvc_2010_x86'] -> Class['windemo::install_msvc_python27'] -> Class['windemo::install_perl5'] -> Class['windemo::install_putty'] ->
+   Class['windemo::install_ultraiso'] -> Class['windemo::install_dx11'] -> Class['windemo::install_msvc_2010_x64'] -> Class['windemo::install_msvc_2010_x86'] ->
+   Class['windemo::install_msvc_python27'] -> Class['windemo::install_python27'] -> Class['windemo::install_perl5'] -> Class['windemo::install_putty'] ->
    Class['windemo::install_bc4'] -> Class['windemo::install_ea10']
 }
