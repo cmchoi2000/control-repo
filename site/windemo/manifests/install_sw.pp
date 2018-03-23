bbd8749dce2913003b2aec3938f0fc0092695019
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-22 20:51:46 -0700

    add set_admin_pwd_ne

--- site/windemo/manifests/install_sw.pp
+++ site/windemo/manifests/install_sw.pp
@@ -1,5 +1,6 @@
 class windemo::install_sw {
   include windemo::enable_rdp
+  include windemo::set_admin_pwd_ne
   include windemo::disable_uac_reg
   include windemo::enable_net_3
   #include windows::set_power_scheme
@@ -18,7 +19,7 @@ class windemo::install_sw {
   include windemo::install_bc4
   include windemo::install_ea10
 
-   Class['windemo::enable_rdp'] -> Class['windemo::disable_uac_reg'] -> Class['windemo::enable_net_3'] -> Class['windemo::set_win_update'] ->
+   Class['windemo::enable_rdp'] -> Class['windemo::set_admin_pwd_ne'] -> Class['windemo::disable_uac_reg'] -> Class['windemo::enable_net_3'] -> Class['windemo::set_win_update'] ->
    Class['windemo::install_ultraiso'] -> Class['windemo::install_dx11'] -> Class['windemo::install_msvc_2010_x64'] -> Class['windemo::install_msvc_2010_x86'] ->
    Class['windemo::install_msvc_python27'] -> Class['windemo::install_python27'] -> Class['windemo::install_perl5'] -> Class['windemo::install_putty'] ->
    Class['windemo::install_bc4'] -> Class['windemo::install_ea10']
