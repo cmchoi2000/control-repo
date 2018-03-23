fdc93dc74c2ed116c3f72f757bc026696d55d7f1
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-23 13:55:19 -0700

    set_vs_config

---
+++ site/windemo/manifests/set_vs_config.pp
@@ -0,0 +1,6 @@
+class windemo::set_vs_config {
+  file {' C:\Users\Administrator\AppData\Local\Microsoft\MSBuild\v4.0\Microsoft.Cpp.x64.user.props':
+  ensure => file,
+  source => 'puppet:///win_share/Software/Microsoft/VS.NET2010/Config/Microsoft.Cpp.x64.user.props',
+  }
+}
