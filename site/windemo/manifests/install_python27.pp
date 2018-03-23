bc86920e53d6cae41ed63827050261e8578f49e7
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-23 13:22:44 -0700

    cython

--- site/windemo/manifests/install_python27.pp
+++ site/windemo/manifests/install_python27.pp
@@ -3,9 +3,17 @@ class windemo::install_python27 {
   ensure => file,
   source => 'puppet:///win_share/Software/Python/python-2.7.10.amd64.msi',
   } ->
+  file {'C:\Install\Cypthon':
+  ensure => directory,
+  source => 'puppet:///win_share/Software/Python/Cypthon',
+  recurse => 'remote'
+  }
   package {'Python 2.7.10 (64-bit)':
   ensure => installed,
   source => 'C:\Install\python-2.7.10.amd64.msi',
   install_options => ['/quiet'],
+  }
+  exec { 'Install Cypthon':
+    command => 'C:\Install\Cypthon\install-cython.cmd',
   }
 }
