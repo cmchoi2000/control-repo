54b487e902579dc534d6c49f6497bf50666996f7
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-23 14:18:11 -0700

    update

--- site/windemo/manifests/install_sw.pp
+++ site/windemo/manifests/install_sw.pp
@@ -18,9 +18,10 @@ class windemo::install_sw {
   #include windemo::install_sab2016
   include windemo::install_bc4
   include windemo::install_ea10
+  include windemo::set_vs_config
 
    Class['windemo::enable_rdp'] -> Class['windemo::set_admin_pwd_ne'] -> Class['windemo::disable_uac_reg'] -> Class['windemo::enable_net_3'] -> Class['windemo::set_win_update'] ->
    Class['windemo::install_ultraiso'] -> Class['windemo::install_dx11'] -> Class['windemo::install_msvc_2010_x64'] -> Class['windemo::install_msvc_2010_x86'] ->
    Class['windemo::install_msvc_python27'] -> Class['windemo::install_python27'] -> Class['windemo::install_perl5'] -> Class['windemo::install_putty'] ->
-   Class['windemo::install_bc4'] -> Class['windemo::install_ea10']
+   Class['windemo::install_bc4'] -> Class['windemo::install_ea10'] -> Class['windemo::set_vs_config']
 }
--- site/windemo/manifests/set_vs_config.pp
+++ site/windemo/manifests/set_vs_config.pp
@@ -1,10 +1,10 @@
 class windemo::set_vs_config {
 	file {'C:\Users\Administrator\AppData\Local\Microsoft\MSBuild':
 	ensure => directory,
-	}
+	} ->
 	file {'C:\Users\Administrator\AppData\Local\Microsoft\MSBuild\v4.0':
 	ensure => directory,
-	}
+	} ->
   file {'C:\Users\Administrator\AppData\Local\Microsoft\MSBuild\v4.0\Microsoft.Cpp.x64.user.props':
   ensure => file,
   source => 'puppet:///win_share/Software/Microsoft/VS.NET2010/Config/Microsoft.Cpp.x64.user.props',
