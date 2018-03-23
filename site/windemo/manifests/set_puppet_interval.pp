abd72c0131dbcb121e14bd3a2dafaa52df7c0408
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-22 21:19:18 -0700

    update

--- site/windemo/manifests/set_puppet_interval.pp
+++ site/windemo/manifests/set_puppet_interval.pp
@@ -1,16 +1,16 @@
-class windemo::install_odac ( 
-   $pathnm = $facts['operatingsystem'] ? { 
-     'windows' => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf', 
-     /(CentOS|RedHat)/ => '/etc/puppetlabs/puppet/puppet.conf', 
-   }, 
-   $runint=3600, 
-   ) 
+class windemo::set_puppet_interval ( 
+  $pathnm = $facts['operatingsystem'] ? { 
+    'windows' => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf', 
+    /(CentOS|RedHat)/ => '/etc/puppetlabs/puppet/puppet.conf', 
+  }, 
+  $runint=3600, 
+  ) 
 { 
-   ini_setting { 'change runinterval': 
-     ensure  => present, 
-     path    => $pathnm, 
-     section => 'agent', 
-     setting => 'runinterval', 
-     value   => $runint, 
-   } 
+  ini_setting { 'change runinterval': 
+    ensure  => present, 
+    path    => $pathnm, 
+    section => 'agent', 
+    setting => 'runinterval', 
+    value   => $runint, 
+  } 
 } 
