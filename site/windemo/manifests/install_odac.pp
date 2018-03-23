9f2a00c98c2f428c1aa6bee4a67d6122ff940f76
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-22 20:41:24 -0700

    fix typo

--- site/windemo/manifests/install_odac.pp
+++ site/windemo/manifests/install_odac.pp
@@ -1,7 +1,7 @@
 class windemo::install_odac {
   file {'C:\Install\ODAC112021Xcopy_x64':
   ensure => directory,
-  source => 'puppet:///win_share/Software/Oracle/10/ODAC112021Xcopy_x64',
+  source => 'puppet:///win_share/Software/Oracle/ODAC112021Xcopy_x64',
   recurse => 'remote'
   }
 #  package {'Windows Software Development Kit - Windows 10.0.16299.91':
