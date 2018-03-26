653272b0f6cbfa7b3be300e562ba64be71beb9a9
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-26 12:37:16 -0700

    Visual Studio Enterprise 2017

--- site/windemo/manifests/install_vs2017_156.pp
+++ site/windemo/manifests/install_vs2017_156.pp
@@ -4,7 +4,7 @@ class windemo::install_vs2017_156 {
   source => 'puppet:///win_share/Software/Microsoft/VS.NET2017_15.6',
   recurse => 'remote'
   } ->
-  package {'Windows Software Development Kit - Windows 10.0.16299.91':
+  package {'Visual Studio Enterprise 2017':
   ensure => installed,
   source => 'C:\Install\VS.NET2017_15.6\vs_Enterprise.exe',
   install_options => ['--in', 'SFS_Dev_Response.json'],
