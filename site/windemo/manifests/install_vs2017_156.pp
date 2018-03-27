3adc02593ec06b26515f6e4d687ec250c665c2b1
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-27 08:39:05 -0700

    install vs2017

--- site/windemo/manifests/install_vs2017_156.pp
+++ site/windemo/manifests/install_vs2017_156.pp
@@ -4,10 +4,10 @@ class windemo::install_vs2017_156 {
   source => 'puppet:///win_share/Software/Microsoft/VS.NET2017_15.6',
   recurse => 'remote'
   }
-#  } ->
-#  package {'Visual Studio Enterprise 2017':
-#  ensure => installed,
-#  source => 'C:\Install\VS.NET2017_15.6\vs_Enterprise.exe',
-#  install_options => ['--in', 'SFS_Dev_Response.json'],
-#  }
+  } ->
+  package {'Visual Studio Enterprise 2017':
+  ensure => installed,
+  source => 'C:\Install\VS.NET2017_15.6\vs_Enterprise.exe',
+  install_options => ['--in', 'SFS_Dev_Response.json'],
+  }
 }
