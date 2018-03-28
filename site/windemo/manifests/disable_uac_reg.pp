9415a0c6e2f2a5ae7552511ee94bfe4d4fe3f428
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 13:13:36 -0700

    update

--- site/windemo/manifests/disable_uac_reg.pp
+++ site/windemo/manifests/disable_uac_reg.pp
@@ -6,8 +6,12 @@ class windemo::disable_uac_reg {
   registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\EnableLUA':
     type => dword,
     data => '0'
-  }
+  } ->
   reboot { 'reboot after uac':
-    when   => 'pending',
+    apply   => finished,
   }
+#    reboot { 'reboot after uac':
+#    subscribe => registry_value,
+#    when   => 'pending',
+#  }
 }
