35e1519122e8d82107d88a1be9c7a0a1d15a4314
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 09:59:21 -0700

    update

--- site/windemo/manifests/install_cypthon.pp
+++ site/windemo/manifests/install_cypthon.pp
@@ -4,8 +4,13 @@ class windemo::install_cypthon {
   source => 'puppet:///win_share/Software/Python/Cypthon',
   recurse => 'remote'
   }
-  exec { 'C:\Install\Cypthon\install-cython.cmd':
+
+  exec { 'Install Cython':
     cwd => 'C:\Install\Cypthon',
+    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\Cypthon\install-cython.cmd"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if exist C:\Python27\Scripts\cython.ex (exit 0) else (exit 1)"',
+#  exec { 'C:\Install\Cypthon\install-cython.cmd':
+#    cwd => 'C:\Install\Cypthon',
 #    command => 'C:\Install\Cypthon\install-cython.cmd',
 #    onlyif   => 'if not exist C:\Python27\Scripts\cython.exe',
   }
