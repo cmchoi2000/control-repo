f6ebf971864c7fdecb2274f9109c29cacec65a25
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 13:32:27 -0700

    SetDesktop

--- site/windemo/manifests/create_folders.pp
+++ site/windemo/manifests/create_folders.pp
@@ -5,4 +5,16 @@ class windemo::create_folders {
 	file {'C:\Jenkins':
 	ensure => directory,
 	}
+	file {'C:\BuildScripts_RTC':
+	ensure => directory,
+	}
+  file {'C:\BuildScripts_RTC\Desktop':
+  ensure => directory,
+  source => 'puppet:///win_share/Software/Desktop',
+  recurse => 'remote'
+	}
+  file {'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\SetDesktop.bat':
+  ensure => file,
+  source => 'puppet:///win_share/Software/Desktop/SetDesktop.bat',
+  }
 }
