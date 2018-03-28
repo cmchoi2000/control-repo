9ef2604f6ca579f9afacf5e14fd051b4e8c76495
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 10:04:02 -0700

    update

--- site/windemo/manifests/install_odac.pp
+++ site/windemo/manifests/install_odac.pp
@@ -4,8 +4,14 @@ class windemo::install_odac {
   source => 'puppet:///win_share/Software/Oracle/ODAC112021Xcopy_x64',
   recurse => 'remote'
   }
-  exec { 'C:\Install\ODAC112021Xcopy_x64\install.bat all c:\oracle odac':
+
+  exec { 'Install ODAC'':
     cwd => 'C:\Install\ODAC112021Xcopy_x64',
+    command => 'C:\Windows\System32\cmd.exe /c "install.bat all c:\oracle odac"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\oracle (exit 0) else (exit 1)"',
+
+#  exec { 'C:\Install\ODAC112021Xcopy_x64\install.bat all c:\oracle odac':
+#    cwd => 'C:\Install\ODAC112021Xcopy_x64',
 #    command => 'C:\Install\ODAC112021Xcopy_x64\install.bat'
 #    provider => windows,
 #    onlyif   => 'if not exist C:\oracle',
