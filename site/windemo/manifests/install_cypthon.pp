05d27c9328bf48f634d27c5f7298cdd83b73e42b
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 10:12:28 -0700

    clean up

--- site/windemo/manifests/install_cypthon.pp
+++ site/windemo/manifests/install_cypthon.pp
@@ -3,15 +3,10 @@ class windemo::install_cypthon {
   ensure => directory,
   source => 'puppet:///win_share/Software/Python/Cypthon',
   recurse => 'remote'
-  }
-
+  } ->
   exec { 'Install Cython':
     cwd => 'C:\Install\Cypthon',
     command => 'C:\Windows\System32\cmd.exe /c "C:\Install\Cypthon\install-cython.cmd"',
     onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Python27\Scripts\cython.exe (exit 0) else (exit 1)"',
-#  exec { 'C:\Install\Cypthon\install-cython.cmd':
-#    cwd => 'C:\Install\Cypthon',
-#    command => 'C:\Install\Cypthon\install-cython.cmd',
-#    onlyif   => 'if not exist C:\Python27\Scripts\cython.exe',
   }
 }
--- site/windemo/manifests/install_odac.pp
+++ site/windemo/manifests/install_odac.pp
@@ -3,17 +3,10 @@ class windemo::install_odac {
   ensure => directory,
   source => 'puppet:///win_share/Software/Oracle/ODAC112021Xcopy_x64',
   recurse => 'remote'
-  }
-
+  } ->
   exec { 'Install ODAC':
     cwd => 'C:\Install\ODAC112021Xcopy_x64',
     command => 'C:\Windows\System32\cmd.exe /c "install.bat all c:\oracle odac"',
     onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\oracle (exit 0) else (exit 1)"',
-
-#  exec { 'C:\Install\ODAC112021Xcopy_x64\install.bat all c:\oracle odac':
-#    cwd => 'C:\Install\ODAC112021Xcopy_x64',
-#    command => 'C:\Install\ODAC112021Xcopy_x64\install.bat'
-#    provider => windows,
-#    onlyif   => 'if not exist C:\oracle',
   }
 }
