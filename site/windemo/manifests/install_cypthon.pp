a4ffaf5e76524fd36c45acf674efac75b47c51b6
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 10:00:59 -0700

    update

--- site/windemo/manifests/install_cypthon.pp
+++ site/windemo/manifests/install_cypthon.pp
@@ -8,7 +8,7 @@ class windemo::install_cypthon {
   exec { 'Install Cython':
     cwd => 'C:\Install\Cypthon',
     command => 'C:\Windows\System32\cmd.exe /c "C:\Install\Cypthon\install-cython.cmd"',
-    onlyif  => 'C:\Windows\System32\cmd.exe /c "if exist C:\Python27\Scripts\cython.ex (exit 0) else (exit 1)"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Python27\Scripts\cython.exe (exit 0) else (exit 1)"',
 #  exec { 'C:\Install\Cypthon\install-cython.cmd':
 #    cwd => 'C:\Install\Cypthon',
 #    command => 'C:\Install\Cypthon\install-cython.cmd',
