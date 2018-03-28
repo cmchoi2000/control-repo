d2ef8070591ddc20a534bd63846ba414bd4c7d22
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 09:51:38 -0700

    ODAC112021Xcopy_x64

--- site/windemo/manifests/install_odac.pp
+++ site/windemo/manifests/install_odac.pp
@@ -4,9 +4,10 @@ class windemo::install_odac {
   source => 'puppet:///win_share/Software/Oracle/ODAC112021Xcopy_x64',
   recurse => 'remote'
   }
-#  exec { 'odac':
+  exec { 'C:\Install\ODAC112021Xcopy_x64\install.bat all c:\oracle odac':
+    cwd => 'C:\Install\ODAC112021Xcopy_x64',
 #    command => 'C:\Install\ODAC112021Xcopy_x64\install.bat'
 #    provider => windows,
 #    onlyif   => 'if not exist C:\oracle',
-#  }
+  }
 }
