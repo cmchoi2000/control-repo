52bd228c9d41c67f16f2325c075dc0fc9c84df82
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-28 13:21:10 -0700

    update

--- site/windemo/manifests/disable_uac_reg.pp
+++ site/windemo/manifests/disable_uac_reg.pp
@@ -1,14 +1,20 @@
 class windemo::disable_uac_reg {
 
-  registry_key { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System':
-      ensure => present,
+#  registry_key { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System':
+#      ensure => present,
+#  }
+  registry::value{'UAC':
+    key    => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System',
+    value  => 'EnableLUA',
+    type   => 'dword',
+    data   => '0',
   }
-  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\EnableLUA':
-    type => dword,
-    data => '0'
-  } ->
+#  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\EnableLUA':
+#    type => dword,
+#    data => '0'
+#  } ->
   reboot { 'reboot after uac':
-    subscribe => registry_value,
+    subscribe => Registry::Value['UAC'],
 #    apply   => finished,
   }
 #    reboot { 'reboot after uac':
