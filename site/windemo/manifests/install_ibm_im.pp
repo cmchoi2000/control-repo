3e5b285e0a037644aaf6fc17dcfad8da9e5315b0
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 11:40:13 -0700

    RTC

--- site/windemo/manifests/install_ibm_im.pp
+++ site/windemo/manifests/install_ibm_im.pp
@@ -1,7 +1,7 @@
 class windemo::install_ibm_im {
   file {'C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip':
   ensure => file,
-  source => 'puppet:///win_share/Software/SCM/agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip',
+  source => 'puppet:///win_share/Software/RTC/agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip',
   }
 #  package {'Microsoft Visual C++ Compiler Package for Python 2.7':
 #  ensure => installed,
