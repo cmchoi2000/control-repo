a4ef949d33dcef277815be4b7bd3b9f0f273c685
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 15:58:02 -0700

    restore

--- site/windemo/manifests/install_ibm_im.pp
+++ site/windemo/manifests/install_ibm_im.pp
@@ -8,7 +8,7 @@ class windemo::install_ibm_im {
     command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip -oC:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417"',
     onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417 (exit 0) else (exit 1)"',
   }
-  package {'IBM Installation Manager t':
+  package {'IBM Installation Manager':
   ensure => installed,
   source => 'C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417\installc.exe',
   install_options => ['-acceptLicense'],
