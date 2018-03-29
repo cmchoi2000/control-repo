d61bb4235f36c676d8f66f810f292b968e69dd37
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 15:14:57 -0700

    install

--- site/windemo/manifests/install_ibm_toolkit.pp
+++ site/windemo/manifests/install_ibm_toolkit.pp
@@ -2,15 +2,19 @@ class windemo::install_ibm_toolkit {
   file {'C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3.zip':
   ensure => file,
   source => 'puppet:///win_share/Software/RTC/RTC-BuildSystem-Toolkit-repo-6.0.3.zip',
-  }
+  } ->
+  file {'C:\Install\IBM_TK.xml':
+  ensure => file,
+  source => 'puppet:///win_share/Software/RTC/IBM_TK.xml',
+  } ->
   exec { 'Extract IBM ToolKit':
     cwd => 'C:\Install\SCM',
     command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3.zip -oC:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3"',
     onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3 (exit 0) else (exit 1)"',
   }
-#  package {'Microsoft Visual C++ Compiler Package for Python 2.7':
-#  ensure => installed,
-#  source => 'C:\Install\VCForPython27.msi',
-#  install_options => ['/quiet', 'ALLUSERS=1'],
-#  }
+  exec { 'Install IBM ToolKit':
+    cwd => 'C:\Program Files\IBM\Installation Manager\eclipse',
+    command => 'C:\Windows\System32\cmd.exe /c "IBMIM.exe -input c:\Install\IBM_TK.xml -acceptLicense -silent -nosplash"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist "C:\Program Files\IBM\Installation Manager\eclipse" (exit 0) else (exit 1)"',
+  }
 }
