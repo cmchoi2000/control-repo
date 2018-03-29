8d20a74002988d8f53e81efe3dd22415546dd1b6
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 14:06:27 -0700

    install im

--- site/windemo/manifests/install_ibm_im.pp
+++ site/windemo/manifests/install_ibm_im.pp
@@ -8,9 +8,9 @@ class windemo::install_ibm_im {
     command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip -oC:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611"',
     onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611 (exit 0) else (exit 1)"',
   }
-#  package {'Microsoft Visual C++ Compiler Package for Python 2.7':
-#  ensure => installed,
-#  source => 'C:\Install\VCForPython27.msi',
-#  install_options => ['/quiet', 'ALLUSERS=1'],
-#  }
+  package {'IBM Installation Manager':
+  ensure => installed,
+  source => 'C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611\installc.exe',
+  install_options => ['-acceptLicense'],
+  }
 }
