d7352aadd1afaf5550b7c499ee3ee3f3fa575bc7
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 11:56:59 -0700

    unzip

--- site/windemo/manifests/install_ibm_im.pp
+++ site/windemo/manifests/install_ibm_im.pp
@@ -2,6 +2,11 @@ class windemo::install_ibm_im {
   file {'C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip':
   ensure => file,
   source => 'puppet:///win_share/Software/RTC/agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip',
+  } ->
+  exec { 'Install IBM IM':
+    cwd => 'C:\Install\SCM',
+    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe a -r C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611 (exit 0) else (exit 1)"',
   }
 #  package {'Microsoft Visual C++ Compiler Package for Python 2.7':
 #  ensure => installed,
