90023607b4613e286f42a42b5c387d237461f8f9
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 15:44:21 -0700

    agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417

--- site/windemo/manifests/install_ibm_im.pp
+++ site/windemo/manifests/install_ibm_im.pp
@@ -1,16 +1,16 @@
 class windemo::install_ibm_im {
-  file {'C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip':
+  file {'C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip':
   ensure => file,
-  source => 'puppet:///win_share/Software/RTC/agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip',
+  source => 'puppet:///win_share/Software/RTC/agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip',
   } ->
   exec { 'Extract IBM IM':
     cwd => 'C:\Install\SCM',
-    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip -oC:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611"',
-    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611 (exit 0) else (exit 1)"',
+    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip -oC:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417 (exit 0) else (exit 1)"',
   }
   package {'IBM Installation Manager':
   ensure => installed,
-  source => 'C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611\installc.exe',
+  source => 'C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417\installc.exe',
   install_options => ['-acceptLicense'],
   }
 }
