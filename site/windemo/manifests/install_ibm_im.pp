5b3ad49b19751155911b5cfcb1f7c260a4032518
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 12:28:33 -0700

    update

--- site/windemo/manifests/install_ibm_im.pp
+++ site/windemo/manifests/install_ibm_im.pp
@@ -3,9 +3,9 @@ class windemo::install_ibm_im {
   ensure => file,
   source => 'puppet:///win_share/Software/RTC/agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip',
   } ->
-  exec { 'Install IBM IM':
+  exec { 'Extract IBM IM':
     cwd => 'C:\Install\SCM',
-    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe a -r C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611"',
+    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611.zip -oC:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611"',
     onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\agent.installer.win32.win32.x86_64_1.8.6000.20161118_1611 (exit 0) else (exit 1)"',
   }
 #  package {'Microsoft Visual C++ Compiler Package for Python 2.7':
--- site/windemo/manifests/install_ibm_toolkit.pp
+++ site/windemo/manifests/install_ibm_toolkit.pp
@@ -3,6 +3,11 @@ class windemo::install_ibm_toolkit {
   ensure => file,
   source => 'puppet:///win_share/Software/RTC/RTC-BuildSystem-Toolkit-repo-6.0.3.zip',
   }
+  exec { 'Extract IBM ToolKit':
+    cwd => 'C:\Install\SCM',
+    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3.zip -oC:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3 (exit 0) else (exit 1)"',
+  }
 #  package {'Microsoft Visual C++ Compiler Package for Python 2.7':
 #  ensure => installed,
 #  source => 'C:\Install\VCForPython27.msi',
