949be96dcd3047ffcd1951cf11e36ea1e4b5e839
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 16:02:19 -0700

    C:\Install\RTC

--- site/windemo/manifests/install_ibm_im.pp
+++ site/windemo/manifests/install_ibm_im.pp
@@ -1,16 +1,21 @@
 class windemo::install_ibm_im {
-  file {'C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip':
+  file {'C:\Install\RTC':
+  ensure => directory,
+  source => 'puppet:///win_share/Software/Desktop',
+  recurse => 'remote'
+	}
+  file {'C:\Install\RTC\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip':
   ensure => file,
   source => 'puppet:///win_share/Software/RTC/agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip',
   } ->
   exec { 'Extract IBM IM':
     cwd => 'C:\Install\SCM',
-    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip -oC:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417"',
-    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417 (exit 0) else (exit 1)"',
+    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\RTC\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417.zip -oC:\Install\RTC\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\RTC\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417 (exit 0) else (exit 1)"',
   }
   package {'IBM Installation Manager':
   ensure => installed,
-  source => 'C:\Install\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417\installc.exe',
+  source => 'C:\Install\RTC\agent.installer.win32.win32.x86_64_1.8.9000.20180313_1417\installc.exe',
   install_options => ['-acceptLicense'],
   }
 }
--- site/windemo/manifests/install_ibm_toolkit.pp
+++ site/windemo/manifests/install_ibm_toolkit.pp
@@ -1,20 +1,20 @@
 class windemo::install_ibm_toolkit {
-  file {'C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3.zip':
+  file {'C:\Install\RTC\RTC-BuildSystem-Toolkit-repo-6.0.3.zip':
   ensure => file,
   source => 'puppet:///win_share/Software/RTC/RTC-BuildSystem-Toolkit-repo-6.0.3.zip',
   } ->
-  file {'C:\Install\IBM_TK.xml':
+  file {'C:\Install\RTC\IBM_TK.xml':
   ensure => file,
   source => 'puppet:///win_share/Software/RTC/IBM_TK.xml',
   } ->
   exec { 'Extract IBM ToolKit':
     cwd => 'C:\Install\SCM',
-    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3.zip -oC:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3"',
-    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\RTC-BuildSystem-Toolkit-repo-6.0.3 (exit 0) else (exit 1)"',
+    command => 'C:\Windows\System32\cmd.exe /c "C:\Install\SCM\7za.exe x C:\Install\RTC\RTC-BuildSystem-Toolkit-repo-6.0.3.zip -oC:\Install\RTC\RTC-BuildSystem-Toolkit-repo-6.0.3"',
+    onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist C:\Install\RTC\RTC-BuildSystem-Toolkit-repo-6.0.3 (exit 0) else (exit 1)"',
   }
   exec { 'Install IBM ToolKit':
     cwd => 'C:\Program Files\IBM\Installation Manager\eclipse',
-    command => 'C:\Windows\System32\cmd.exe /c "IBMIM.exe -input c:\Install\IBM_TK.xml -acceptLicense -silent -nosplash"',
+    command => 'C:\Windows\System32\cmd.exe /c "IBMIM.exe -input c:\Install\RTC\IBM_TK.xml -acceptLicense -silent -nosplash"',
     onlyif  => 'C:\Windows\System32\cmd.exe /c "if not exist "C:\Program Files\IBM\TeamConcertBuild" (exit 0) else (exit 1)"',
   }
 }
