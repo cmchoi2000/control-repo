b4ca322ce42f8793e90740abfcec8216daad1f3d
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-29 23:03:33 -0700

    update

--- site/windemo/manifests/enable_net_35.pp
+++ site/windemo/manifests/enable_net_35.pp
@@ -3,7 +3,7 @@ class windemo::enable_net_35 {
     ensure => directory,
     source => 'puppet:///win_share/Software/Microsoft/DotNet/3.5_sxs',
     recurse => 'remote'
-  }
+  } ->
     dism { 'NetFx3':
     ensure => present,
     all    => true,
