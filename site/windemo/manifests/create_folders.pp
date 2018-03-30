fdd2b5ca0d08ffc1402724ea1cb73bc08733a2bf
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-03-30 08:54:39 -0700

    add folders

--- site/windemo/manifests/create_folders.pp
+++ site/windemo/manifests/create_folders.pp
@@ -2,10 +2,25 @@ class windemo::create_folders {
 	file {'C:\Install':
 	  ensure => directory,
 	}
+	file {'C:\BuildScripts_RTC':
+	  ensure => directory,
+	}
 	file {'C:\Jenkins':
 	  ensure => directory,
 	}
 	file {'C:\Temp':
+  	ensure => directory,
+	}
+	file {'C:\RTC_Builds':
+  	ensure => directory,
+	}
+	file {'C:\ECTemp':
+  	ensure => directory,
+	}
+	file {'C:\IMC':
+  	ensure => directory,
+	}
+	file {'C:\NGS':
   	ensure => directory,
 	}
   file {'C:\Install\Desktop':
