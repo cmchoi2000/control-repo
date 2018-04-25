2323e843f6e78ef20d5a9bc72bff699ac754f42a
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-04-24 17:11:13 -0700

    disable_nfs_reg

---
+++ site/windemo/manifests/disable_nfs_reg.pp
@@ -0,0 +1,14 @@
+class windemo::disable_nfs_reg {
+
+  registry::value{'LanmanServer':
+    key    => 'HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters',
+    value  => 'Size',
+    type   => 'dword',
+    data   => '3',
+  }
+  registry::value{'Memory':
+    key    => 'HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management',
+    value  => 'LargeSystemCache',
+    type   => 'dword',
+    data   => '1',
+  }
