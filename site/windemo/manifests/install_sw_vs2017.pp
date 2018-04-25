b523eb9678dd7c40ee85d8204144e0471bc4af2f
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-04-25 13:10:42 -0700

    vs2017

---
+++ site/windemo/manifests/install_sw_vs2017.pp
@@ -0,0 +1,20 @@
+class windemo::install_sw_vs2017 {
+  include windemo::enable_rdp
+  include windemo::set_admin_pwd_ne
+  include windemo::disable_uac_reg
+  #include windows::set_power_scheme
+  include windemo::set_win_update
+  include windemo::install_vs2017_156
+  include windemo::install_ibm_im
+  include windemo::install_ibm_toolkit
+  include windemo::install_perl5
+
+   Class['windemo::enable_rdp'] ->
+   Class['windemo::set_admin_pwd_ne'] ->
+   Class['windemo::disable_uac_reg'] ->
+   Class['windemo::set_win_update'] ->
+   Class['windemo::install_vs2017_156'] ->
+   Class['windemo::install_ibm_im'] ->
+   Class['windemo::install_ibm_toolkit'] ->
+   Class['windemo::install_perl5']
+}
