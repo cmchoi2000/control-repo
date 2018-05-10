2738f295b299ddc8ae4c635e4e47bfbba848ffee
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-05-10 15:38:30 -0700

    use 15.7.1

--- site/windemo/manifests/install_vs2017_1571.pp
+++ site/windemo/manifests/install_vs2017_1571.pp
@@ -1,15 +1,22 @@
 class windemo::install_vs2017_1571 {
-  file {'C:\Install\VS.NET2017_15.7.1':
-  ensure => directory,
-  source => 'puppet:///win_share/Software/Microsoft/VS.NET2017_15.7.1',
-  recurse => 'remote'
+  file {'Remove VS.NET2017_15.6':
+    ensure => absent,
+    path => 'C:\Install\VS.NET2017_15.6',
+    recurse => true,
+    purge => true,
+    force => true
   } ->
+  file {'C:\Install\VS.NET2017_15.7.1':
+    ensure => directory,
+    source => 'puppet:///win_share/Software/Microsoft/VS.NET2017_15.7.1',
+    recurse => 'remote'
+  } ->  
   package {'Visual Studio Enterprise 2017':
-  ensure => installed,
-  source => 'C:\Install\VS.NET2017_15.7.1\vs_Enterprise.exe',
-  install_options => ['--in', 'C:\Install\VS.NET2017_15.7.1\SFS_SCM_Response.json', '--wait'],
+    ensure => installed,
+    source => 'C:\Install\VS.NET2017_15.7.1\vs_Enterprise.exe',
+    install_options => ['--in', 'C:\Install\VS.NET2017_15.7.1\SFS_SCM_Response.json', '--wait'],
   }
-  reboot { 'Reboot After VS2017':
+  reboot {'Reboot After VS2017':
     subscribe => Package['Visual Studio Enterprise 2017'],
   }
 }
