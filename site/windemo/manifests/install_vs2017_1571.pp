2a9b1dc36c333381d63b2b1ad62f6ab614ca822e
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-05-10 14:43:59 -0700

    add

---
+++ site/windemo/manifests/install_vs2017_1571.pp
@@ -0,0 +1,15 @@
+class windemo::install_vs2017_1571 {
+  file {'C:\Install\VS.NET2017_15.7.1':
+  ensure => directory,
+  source => 'puppet:///win_share/Software/Microsoft/VS.NET2017_15.7.1',
+  recurse => 'remote'
+  } ->
+  package {'Visual Studio Enterprise 2017':
+  ensure => installed,
+  source => 'C:\Install\VS.NET2017_15.7.1\vs_Enterprise.exe',
+  install_options => ['--in', 'C:\Install\VS.NET2017_15.7.1\SFS_SCM_Response.json', '--wait'],
+  }
+  reboot { 'Reboot After VS2017':
+    subscribe => Package['Visual Studio Enterprise 2017'],
+  }
+}
