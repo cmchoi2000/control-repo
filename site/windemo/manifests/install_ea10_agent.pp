613644ad15ad69cca0fe91ff5488dd53f2b10264
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-05-10 17:39:05 -0700

    10.0.3.78576

--- site/windemo/manifests/install_ea10_agent.pp
+++ site/windemo/manifests/install_ea10_agent.pp
@@ -2,9 +2,9 @@ class windemo::install_ea10_agent {
 	file {'C:\Install\ElectricAccelerator':
 	ensure => directory,
 	}
-  file {'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.1.78161-Windows-Install.exe':
+  file {'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.3.78576-Windows-Install.exe':
   ensure => file,
-  source => 'puppet:///win_share/Software/ElectricCloud/10.0.1.78161/windows/ElectricAccelerator-10.0.1.78161-Windows-Install.exe',
+  source => 'puppet:///win_share/Software/ElectricCloud/10.0.3.78576/windows/ElectricAccelerator-10.0.3.78576-Windows-Install.exe',
   } ->
   file {'C:\Install\ElectricAccelerator\install10.props':
   ensure => file,
@@ -12,7 +12,7 @@ class windemo::install_ea10_agent {
   } ->
   package {'ElectricAccelerator':
   ensure => installed,
-  source => 'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.1.78161-Windows-Install.exe',
+  source => 'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.3.78576-Windows-Install.exe',
   install_options => ['--mode', 'silent', '--propertyfile', 'C:\Install\ElectricAccelerator\install10.props'],
   } ->
   file {'C:\ECloud\i686_win32\bin\runagent.local':
--- site/windemo/manifests/install_ea10_emake.pp
+++ site/windemo/manifests/install_ea10_emake.pp
@@ -2,13 +2,13 @@ class windemo::install_ea10_emake {
 	file {'C:\Install\ElectricAccelerator':
 	ensure => directory,
 	}
-  file {'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.1.78161-Windows-Install.exe':
+  file {'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.3.78576-Windows-Install.exe':
   ensure => file,
-  source => 'puppet:///win_share/Software/ElectricCloud/10.0.1.78161/windows/ElectricAccelerator-10.0.1.78161-Windows-Install.exe',
+  source => 'puppet:///win_share/Software/ElectricCloud/10.0.3.78576/windows/ElectricAccelerator-10.0.3.78576-Windows-Install.exe',
   } ->
   package {'ElectricAccelerator':
   ensure => installed,
-  source => 'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.1.78161-Windows-Install.exe',
+  source => 'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.3.78576-Windows-Install.exe',
   install_options => ['--mode', 'silent'],
   }
 }
