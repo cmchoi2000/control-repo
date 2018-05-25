d57d5503826c897bd2fa8a434075a4865d2dd6a9
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-05-24 19:05:31 -0700

    update

--- site/windemo/manifests/install_ktpython27.pp
+++ site/windemo/manifests/install_ktpython27.pp
@@ -1,5 +1,8 @@
 class windemo::install_ktpython27 {
-  file {'C:\Install\Python\Python_2.7.10156.iso':
+	file {'C:\Install\KT_Python':
+	ensure => directory,
+	} ->
+  file {'C:\Install\KT_Python\Python_2.7.10156.iso':
   ensure => file,
   source => 'puppet:///win_share/Software/KT/Python/Python_2.7.10156.iso',
   }
