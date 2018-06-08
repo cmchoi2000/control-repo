f128847e61a760d92c33c08a70d8d4df8aca113f
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-06-07 18:01:53 -0700

    update

--- site/windemo/manifests/install_ktaddin_vs2010.pp
+++ site/windemo/manifests/install_ktaddin_vs2010.pp
@@ -1,5 +1,5 @@
 class windemo::install_ktaddin_vs2010 {
-  file {'C:\Install\KTAddinBuildEnv':
+  file {'C:\Install\KTAddinBuildEnv_VS2010':
   ensure => directory,
   source => 'puppet:///win_share/Software/KTAddinBuildEnv/Current NGS 7.00/1.27.0.2010',
   recurse => 'remote'
