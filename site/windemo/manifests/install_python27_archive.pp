bdfce22dcb82d3fea76f892dc0f621df1ed35e88
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-06-28 13:21:44 -0700

    test

--- site/windemo/manifests/install_python27_archive.pp
+++ site/windemo/manifests/install_python27_archive.pp
@@ -1,5 +1,8 @@
 class windemo::install_python27_archive {
 
+  $message = lookup('message')
+  notify { $message: }
+
   archive {'C:\Install\Python':
   ensure        => present,
 #  extract       => true,
@@ -8,9 +11,5 @@ class windemo::install_python27_archive {
   source => 'puppet:///win_share/Software/Python/python-2.7.10.amd64.msi',
   }
 
-  archive {'C:\Install\ODAC112021Xcopy_x64':
-  ensure => present,
-  source => 'puppet:///win_share/Software/Oracle/ODAC112021Xcopy_x64',
-  }
 
 }
