1263353817d37215c540553dc6ca6fadaeeb2a45
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-06-27 17:56:02 -0700

    update

--- site/windemo/manifests/install_python27_archive.pp
+++ site/windemo/manifests/install_python27_archive.pp
@@ -1,8 +1,8 @@
 class windemo::install_python27_archive {
 
-  archive {'C:\Install\Python\python-2.7.10.amd64.msi':
+  archive {'C:\Install\Python':
   ensure        => present,
-  extract       => true,
+#  extract       => true,
 #  source        => '/usr/win/Software/Python/python-2.7.10.amd64.msi',
 #  source        => 'C:\temp\python-2.7.10.amd64.msi',
   source => 'puppet:///win_share/Software/Python/python-2.7.10.amd64.msi',
