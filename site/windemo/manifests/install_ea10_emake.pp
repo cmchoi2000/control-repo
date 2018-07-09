9dc5f7e95e8fc7b45f72d2204c35432dae601377
cmchoi2000 <35359333+cmchoi2000@users.noreply.github.com>
2018-07-09 13:05:28 -0700

    10.1.0.80918

--- site/windemo/manifests/install_ea10_emake.pp
+++ site/windemo/manifests/install_ea10_emake.pp
@@ -3,26 +3,25 @@ class windemo::install_ea10_emake {
 	file {'C:\Install\ElectricAccelerator':
     ensure => directory,
 	} ->
-	file {'C:\Install\ElectricAccelerator\10.0.3.78576':
+	file {'C:\Install\ElectricAccelerator\10.1.0.80918':
     ensure => directory,
 	} ->  
+  file {'C:\Install\ElectricAccelerator\10.0.3.78576':
+    ensure => absent,
+  } ->
   file {'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.1.78161-Windows-Install.exe':
     ensure => absent,
   } ->
   file {'C:\Install\ElectricAccelerator\ElectricAccelerator-10.0.3.78576-Windows-Install.exe':
     ensure => absent,
   } ->
-  file {'C:\Install\ElectricAccelerator\10.0.3.78576\VSIntegration-5.2.5.39245-Install.exe':
+  file {'C:\Install\ElectricAccelerator\10.1.0.80918\ElectricAccelerator-10.1.0.80918-Windows-Install.exe':
     ensure => file,
-    source => 'puppet:///win_share/Software/ElectricCloud/VSPlugin/5.2.5.39245/VSIntegration-5.2.5.39245-Install.exe',
-  } ->
-  file {'C:\Install\ElectricAccelerator\10.0.3.78576\ElectricAccelerator-10.0.3.78576-Windows-Install.exe':
-    ensure => file,
-    source => 'puppet:///win_share/Software/ElectricCloud/10.0.3.78576/windows/ElectricAccelerator-10.0.3.78576-Windows-Install.exe',
+    source => 'puppet:///win_share/Software/ElectricCloud/10.1.0.80918/windows/ElectricAccelerator-10.1.0.80918-Windows-Install.exe',
   } ->
   package {'ElectricAccelerator':
     ensure => installed,
-    source => 'C:\Install\ElectricAccelerator\10.0.3.78576\ElectricAccelerator-10.0.3.78576-Windows-Install.exe',
+    source => 'C:\Install\ElectricAccelerator\10.1.0.80918\ElectricAccelerator-10.1.0.80918-Windows-Install.exe',
     install_options => ['--mode', 'silent'],
   }
 }
